824d29be1d945cdc86b56872078102b4d9489793
Karan Singh <115371877+itsrealkaran@users.noreply.github.com>
2024-09-14 19:27:17 +0000

    shit hit

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,50 @@ import { Physics } from "@react-three/rapier";
 import { Suspense, useState } from "react";
 import { Experience } from "./components/Experience";
 import { Leaderboard } from "./components/Leaderboard";
+import { ConnectButton, useConnection } from 'arweave-wallet-kit'
 
 function App() {
   const [downgradedPerformance, setDowngradedPerformance] = useState(false);
+  const { connected } = useConnection();
+console.log(connected);
   return (
     <>
-      <Loader />
-      <Leaderboard />
-      <Canvas
-        shadows
-        camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
-        dpr={[1, 1.5]} // optimization to increase performance on retina/4k devices
-      >
-        <color attach="background" args={["#242424"]} />
-        <SoftShadows size={42} />
 
-        <PerformanceMonitor
-          // Detect low performance devices
-          onDecline={(fps) => {
-            setDowngradedPerformance(true);
-          }}
-        />
-        <Suspense>
-          <Physics>
-            <Experience downgradedPerformance={downgradedPerformance} />
-          </Physics>
-        </Suspense>
-        {!downgradedPerformance && (
-          // disable the postprocessing on low-end devices
-          <EffectComposer disableNormalPass>
-            <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
-          </EffectComposer>
-        )}
-      </Canvas>
+      {
+        connected ? (
+          <>
+            <Loader />
+            <Leaderboard />
+            <Canvas
+              shadows
+              camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
+              dpr={[1, 1.5]} // optimization to increase performance on retina/4k devices
+            >
+              <color attach="background" args={["#242424"]} />
+              <SoftShadows size={42} />
+
+              <PerformanceMonitor
+                // Detect low performance devices
+                onDecline={(fps) => {
+                  setDowngradedPerformance(true);
+                }}
+              />
+              <Suspense>
+                <Physics>
+                  <Experience downgradedPerformance={downgradedPerformance} />
+                </Physics>
+              </Suspense>
+              {!downgradedPerformance && (
+                // disable the postprocessing on low-end devices
+                <EffectComposer disableNormalPass>
+                  <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+                </EffectComposer>
+              )}
+            </Canvas>
+          </>) : (
+          <ConnectButton profileModal={true} showBalance={true} showAddress={false} showProfilePicture={false} />
+        )
+      }
     </>
   );
 }
